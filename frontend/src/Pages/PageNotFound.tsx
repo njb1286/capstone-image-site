6d2c3f0c955d34fa6a10876413b0ac81078e8662
Njb1286 <bennionnj@gmail.com>
2024-01-26 12:22:22 -0700

    Added control over the link in the PageNotFound component

--- frontend/src/Pages/PageNotFound.tsx
+++ frontend/src/Pages/PageNotFound.tsx
@@ -4,15 +4,27 @@ import { FaSearch } from "react-icons/fa";
 
 type PageNotFoundProps = {
   message: string;
+  link?: {
+    text: string;
+    path: string;
+  }
 }
 
-const PageNotFound = ({ message }: Readonly<PageNotFoundProps>) => {
+const PageNotFound = (props: Readonly<PageNotFoundProps>) => {
+  let path = "/";
+  let text = "Return to home";
+
+  if (props.link) {
+    path = props.link.path;
+    text = props.link.text;
+  }
+  
   return (
     <div className={classes.container}>
       <div className={classes.content}>
         <FaSearch className={classes.icon} />
-        <p className={classes.message}>{message}</p>
-        <NavLink className={classes.link} to="/">Return to home</NavLink>
+        <p className={classes.message}>{props.message}</p>
+        <NavLink className={classes.link} to={path}>{text}</NavLink>
       </div>
     </div>
   )
